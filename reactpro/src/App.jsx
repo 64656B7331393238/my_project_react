ff7aaa2aeb2877fee0ea2d4b4340c52ad2a63919
Dekston <wailun0519@gmail.com>
2024-05-10 11:08:41 +0800

    commit

--- reactpro/src/App.jsx
+++ reactpro/src/App.jsx
@@ -1,10 +1,11 @@
 import Hello from './components/Hello';
 import Fruits from './components/Fruits';
 import ConditionalComponent from './components/ConditionalComponent';
+import Message from './components/Message';
 function App() {
   return (
     <div className="App">
-      <Fruits />
+      <Message />
     </div>
   );
 }
